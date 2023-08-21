12c77d256a82fb868a0ad07dec29f426471de68c
vkopallx <Venkata.Kopalle@solidigm.com>
2023-08-21 14:52:01 +0530

    modified package_install script

--- package_install.sh
+++ package_install.sh
@@ -1,13 +1,24 @@
 #! /bin/bash
 
-list_of_packages = $@
+LIST_OF_PACKAGE= $@
+
+VALIDATE(){
+    if [ $2 -gt 0 ]
+    then 
+        echo "$1 installation FAILED"
+    else    
+        exho "$1 installation SUCCESS"
+    if
+}
 
 if [ $(id -u) -eq 0 ]
 then
-    for package in $list_of_packages
+    for package in $LIST_OF_PACKAGE
     do
         sudo yum install package -y
+        VALIDATE $package $?
     done
 else
     echo "Run using sudo user"
+    exit 1
 if
