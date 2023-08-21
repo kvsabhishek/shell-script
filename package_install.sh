77eff36b4bcfa8ebbd4d053e2b8a8c3503612ea4
vkopallx <Venkata.Kopalle@solidigm.com>
2023-08-21 15:07:29 +0530

    modified package_install script

--- package_install.sh
+++ package_install.sh
@@ -2,13 +2,20 @@
 
 LIST_OF_PACKAGE= $@
 USERID= $(id -u)
+LOGFILE=/tmp/$(date %+F).log
+
+R="\e[31m"
+N="\e[0m"
+G="\e[32m]"
+
+
 
 VALIDATE(){
     if [ $2 -gt 0 ]
     then 
-        echo "$1 installation ....... FAILED"
+        echo "$1 installation ....... $R FAILED $N"
     else    
-        exho "$1 installation ....... SUCCESS"
+        exho "$1 installation ....... $G SUCCESS $N"
     fi
 }
 
@@ -16,8 +23,8 @@ if [ $USERID -eq 0 ]
 then
     for PACKAGE in $LIST_OF_PACKAGE
     do
-        yum install $PACKAGE -y
-        VALIDATE $PACKAGE $?
+        yum install $PACKAGE -y &>>$LOGFILE
+        VALIDATE "$PACKAGE" $?
     done
 else
     echo "Run using sudo user"
