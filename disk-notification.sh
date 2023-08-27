3e0d7e0159f89dab9ad5e9f0d908a52b2635b57a
Abhishek <kvsabhiakhi@gmail.com>
2023-08-27 11:54:43 +0530

    added disk usage notification script

---
+++ disk-notification.sh
@@ -0,0 +1,14 @@
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$(date +%F-%Hh-%Mm-%Ss).log
+
+R="\e[31m"
+N="\e[0m"
+G="\e[32m]"
+
+DISK_USAGE=$(df -hT | grep -vE "tmpfs|Filesystem")
+DISK_USAGE_THRESHOLD=1
+
+while IFS= read line
+do 
+    usage=$(echo $line)
+done <<< $DISK_USAGE
