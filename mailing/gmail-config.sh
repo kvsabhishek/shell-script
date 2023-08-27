846552ed8756ca9ed0371d661dcf99d94824dde4
Abhishek <kvsabhiakhi@gmail.com>
2023-08-27 19:35:25 +0530

    modified gamil configuration

--- mailing/gmail-config.sh
+++ mailing/gmail-config.sh
@@ -7,7 +7,7 @@ LOG_DIR=/tmp
 DATE=$(date +%F-%Hh-%Mm-%Ss)
 LOG_FILE=$LOG_DIR/$0-$DATE.log
 
-CONFIG_LINES=(relayhost = [smtp.gmail.com]:587, smtp_use_tls = yes, smtp_sasl_auth_enable = yes, smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd, smtp_sasl_security_options = noanonymous, smtp_sasl_tls_security_options = noanonymous)
+CONFIG_LINES=("relayhost = [smtp.gmail.com]:587" "smtp_use_tls = yes" "smtp_sasl_auth_enable = yes" "smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd" "smtp_sasl_security_options = noanonymous" "smtp_sasl_tls_security_options = noanonymous")
 read -p "Enter gmail password: " PASS
 read -p "Enter gmail mail id: " MAIL
 SASL_PASSWD="[smtp.gmail.com]:587 $MAIL:$PASS"
