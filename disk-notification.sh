4fa8e73fcf3ffab3ab382197201bf4931afba157
Abhishek <kvsabhiakhi@gmail.com>
2023-08-27 12:05:21 +0530

    modified disk-notification script

--- disk-notification.sh
+++ disk-notification.sh
@@ -3,7 +3,8 @@ LOGFILE=/tmp/$SCRIPT_NAME-$(date +%F-%Hh-%Mm-%Ss).log
 
 R="\e[31m"
 N="\e[0m"
-G="\e[32m]"
+G="\e[32m"
+Y="\e[33m"
 
 DISK_USAGE=$(df -hT | grep -vE "tmpfs|Filesystem")
 DISK_USAGE_THRESHOLD=1
@@ -12,4 +13,9 @@ while IFS= read line
 do 
     disk_name=$(echo $line | awk '{print $1}')
     disk_usage=$(echo $line | awk '{print $1}' | cut -d % -f1)
+
+    if [ disk_usage -gt $DISK_USAGE_THRESHOLD ]
+    then
+        echo "$Y WARNING::: Disk $disk_name usage is $disk_usage which is above threshhold $DISK_USAGE_THRESHOLD $N"
+    fi
 done <<< $DISK_USAGE
