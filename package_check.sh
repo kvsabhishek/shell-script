ecacff21c2f1ce0e4379d99d9ec686f643ca0260
vkopallx <Venkata.Kopalle@solidigm.com>
2023-08-30 18:30:31 +0530

    modified package_check.sh

--- package_check.sh
+++ package_check.sh
@@ -7,3 +7,8 @@ if yum list installed $PACKAGE_NAME >/dev/null 2>&1; then
 else
     echo "$PACKAGE_NAME is not installed."
 fi
+
+
+#yum list installed $PACKAGE_NAME >/dev/null 2>&1   ------> This retunrs only the status code of 
+#   $? = 0 ------> If package installed
+#   $? = 1 ------> If package not found/installed
