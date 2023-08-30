2f9d2e986d6386ac4ae819ebfc45cbfe8ac528b4
vkopallx <Venkata.Kopalle@solidigm.com>
2023-08-30 18:28:35 +0530

    added package_check.sh

--- package_check.sh
+++ package_check.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 read -p "Enter package name" PACKAGE_NAME
-PACKAGE_NAME="package-name"
 
 if yum list installed $PACKAGE_NAME >/dev/null 2>&1; then
     echo "$PACKAGE_NAME is installed."
